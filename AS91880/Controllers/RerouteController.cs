5b2e77a9b60f6261b186c40a125c347c2b0fc218
Bradley Grover <ac111897@avcol.school.nz>
2021-11-02 12:29:04 +1300

    Add project files.

---
+++ AS91880/Controllers/RerouteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AS91880.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RerouteController : ControllerBase
+    {
+        private static readonly string _siteHome = "/Home";
+        /// <summary>
+        /// Reroutes "/" to the home page
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/")]
+        public IActionResult RerouteMain()
+        {
+            return RedirectToPage(_siteHome);
+        }
+        /// <summary>
+        /// Reroutes "/index" to the home page
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/index")]
+        public IActionResult RerouteIndex()
+        {
+            return RedirectToPage(_siteHome);
+        }
+    }
+}
---
+++ AS91880/Services/PrivacyService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AS91880.Services
+{
+    public class PrivacyService
+    {
+        private readonly string content = null;
+        private ILogger<PrivacyService> _logger;
+        public PrivacyService(IConfiguration config, ILogger<PrivacyService> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation($"Got privacy at {Directory.GetCurrentDirectory() + config["privacy-path"]}");
+            content = File.ReadAllText(Directory.GetCurrentDirectory() + config["privacy-path"]);
+        }
+        public string Content
+        {
+            get
+            {
+
+                return content
+                    ??
+#if (DEBUG)
+                    "Couldn't find content";
+#else
+                    string.Empty;
+#endif
+
+
+            }
+        }
+    }
+}

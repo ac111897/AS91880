a6c15f8512e2eb04c5c3e5c1e6344646d9b727e1
Bradley Grover <ac111897@avcol.school.nz>
2021-11-05 15:09:30 +1300

    Added 404 reroute controller and added base image to the myth page as well as some written content

---
+++ AS91880/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AS91880.Controllers
+{
+    [Route("[controller]")]
+    public class ErrorController : ControllerBase
+    {
+        [Route("404")]
+        public IActionResult PageNotFound()
+        {
+            return RedirectToPage("/NotFound");
+        }
+    }
+}
--- AS91880/Startup.cs
+++ AS91880/Startup.cs
@@ -43,7 +43,15 @@ namespace AS91880
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/NotFound";
+                    await next();
+                }
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 

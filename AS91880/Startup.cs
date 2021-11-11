0afb6ab14ad52137df58bfc31bb341d0965fc195
Bradley Grover <ac111897@avcol.school.nz>
2021-11-11 19:59:33 +1300

    add more comments

--- AS91880/Startup.cs
+++ AS91880/Startup.cs
@@ -43,7 +43,7 @@ namespace AS91880
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             // Reroute 404's to /NotFound page
             app.Use(async (context, next) =>
             {
